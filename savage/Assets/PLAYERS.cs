d5bb912a5d4e12a52c37717fbc746a89f4ec45e1
unknown <evangelinecastro123@gmail.com>
2022-10-05 02:30:17 +0800

    4

--- savage/Assets/PLAYERS.cs
+++ savage/Assets/PLAYERS.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
 
 public class PLAYERS : MonoBehaviour
 {
+
+    public TMP_InputField firstPlayer;
+    public TMP_InputField secondPLayer;
+    public TMP_InputField playerHp;
     // Start is called before the first frame update
     public GameObject videoplayerGO;
 
@@ -15,4 +22,16 @@ void Start()
     videoplayerGO.gameObject.GetComponent<VideoPlayer>().clip = vc1;
     videoplayerGO.gameObject.GetComponent<VideoPlayer>().Play();
 }
+public void setVariable()
+{
+    dataHandler.vPasser.pName1 = firstPlayer.text;
+    dataHandler.vPasser.pName2 = secondPLayer.text;
+    dataHandler.vPasser.pHealth = System.Convert.ToInt32(playerHp.text);
+    SceneManager.LoadScene(2);
 }
+
+}
+
+
+
+
---
+++ savage/Assets/dataHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dataHandler: MonoBehaviour
+{   
+    public static dataHandler vPasser;
+    public string pName1;
+    public string pName2;
+    public int pHealth;
+
+    public int winner;
+
+    
+
+    private void Awake()
+    {
+        if (vPasser == null)
+        {
+            vPasser = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
